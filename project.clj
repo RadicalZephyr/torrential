0fdf0eb3f0295bda4e3cc84fdb52f823c337e69c
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-23 02:39:36 -0500

    Fill out project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject torrential "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A minimalist Clojure BitTorrent client."
+  :url "https://github.com/RadicalZephyr/torrential"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
