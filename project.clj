d8bff1a0223a5c91a1f1e165e4686836ddad6f2d
Geoff Shannon <geoffpshannon@gmail.com>
2015-05-23 03:09:24 -0500

    Add scm entry to project.clj

--- project.clj
+++ project.clj
@@ -3,6 +3,8 @@
   :url "https://github.com/RadicalZephyr/torrential"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "https://github.com/RadicalZephyr/torrential.git"}
 
   :dependencies [[org.clojure/clojure "1.6.0"]]
 
@@ -10,8 +12,7 @@
 
   :plugins [[speclj "3.2.0"]]
 
-  :profiles {:dev {:dependencies [[speclj "3.2.0"]]
-                   }
+  :profiles {:dev {:dependencies [[speclj "3.2.0"]]}
              :uberjar {:aot :all}}
 
   :main ^:skip-aot torrential.core
